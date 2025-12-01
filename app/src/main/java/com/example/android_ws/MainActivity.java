3677d8026fd1bf462f9cf7efaf3ce4b517632c74
mankit <mankit556@gmail.com>
2025-12-01 22:11:16 +0800

    commit

--- app/src/main/java/com/example/android_ws/MainActivity.java
+++ app/src/main/java/com/example/android_ws/MainActivity.java
@@ -1,11 +1,20 @@
-package com.example.andorid_ws;
+package com.example.android_ws;
 
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
+        TextView textView = new TextView(this);
+        textView.setText("Hello, World!");
+        textView.setTextSize(30);
 
+        
+        setContentView(textView);
+    }
 }
