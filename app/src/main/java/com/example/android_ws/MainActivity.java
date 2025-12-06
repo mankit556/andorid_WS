93b22c2e141ee03a4de7eabc6248a80d7a36c4f1
mankit <mankit556@gmail.com>
2025-12-06 21:29:29 +0800

    edit layout

--- app/src/main/java/com/example/android_ws/MainActivity.java
+++ app/src/main/java/com/example/android_ws/MainActivity.java
@@ -4,17 +4,38 @@ import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final ConcurrentHashMap<String , Object> MEMORY_STORE = new ConcurrentHashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.layoutui);
+    }
 
-        TextView textView = new TextView(this);
-        textView.setText("Hello, World!");
-        textView.setTextSize(30);
+    public static void put(String key, Object value){
+        if (key == null)return;
+        if (value == null){
+            MEMORY_STORE.remove(key);
+        }else{
+            MEMORY_STORE.put(key,value);
+        }
+    }
 
-        
-        setContentView(textView);
+    @SuppressWarnings("unchecked")
+    public static<T> T get(String key){
+        return (T) MEMORY_STORE.get(key);
+    }
+
+    public static void remove(String key){
+        MEMORY_STORE.remove(key);
+    }
+
+    public static boolean contains(String key){
+        return MEMORY_STORE.containsKey(key);
     }
 }
